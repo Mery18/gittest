1149b6773c356399f7aee1ad82d89baec73a8cf2
Mery18 <marianiemiec@wp.pl>
2017-03-03 07:57:06 +0100

    Szyfr cezara v1

---
+++ cpp/szyfr_cezara.cpp
@@ -0,0 +1,24 @@
+/*
+ * szyfr_cezara.cpp
+ *
+ *
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+
+{   char tekst[100];
+	int klucz =3;
+    cout << "Podaj tekst:"<< endl;
+    cin >> tekst;
+    cout <<"Podaj klucz: ";
+    cin >> klucz;
+    szyfruj(tekst, klucz);
+    cout <<tekst;
+	return 0;
+}
+
