2959db9c1494e58e72e990b4056800b7eb54f867
Mery18 <marianiemiec@wp.pl>
2017-03-03 07:21:21 +0100

    Jakaś zmiana

--- python/kodmorsa.py
+++ python/kodmorsa.py
@@ -3,14 +3,14 @@
 
 
 def koduj(napis):
-    litery = {'a': '*-', 'b': '-***'}
+    litery = {'a': '*-', 'b': '-***', 'c': '-*–*','d': '–**', 'e': '*', 'f': '**–*', 'g': '–**','h': '****', 'i': '**', 'j': '*–––', 'k': '–*–', 'l': '*–**', 'm': '––', 'n': '–*', 'o': '–––', 'p': '*––*', 'r': '*–*', 's': '***', 't':'–', 'u': '**–', 'w': '*––', 'v': '***–', 'x': '–**–', 'y': '–*–', 'z': '––**'}
     for litera in napis:
         if litera != ' ':
-            print litery[litera]
+            print litery[litera.lower()]
 
 def main(args):
-    #pobierz_napis
-    koduj("abc")
+    napis = raw_input("Podaj napis")
+    koduj("napis")
     return 0
 
 
