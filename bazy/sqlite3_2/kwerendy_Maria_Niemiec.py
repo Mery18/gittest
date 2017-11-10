a8be28d3450766dc5e1fe8ad4d11746402074f12
Mery18 <kl3ag1@komp05.lo1cg.org>
2017-11-10 07:54:01 +0100

    Piatek 10.11

--- bazy/sqlite3_2/kwerendy_Maria_Niemiec.py
+++ bazy/sqlite3_2/kwerendy_Maria_Niemiec.py
@@ -4,6 +4,14 @@
 
 import sqlite3
 
+import os
+from peewee import *
+from dane import *
+
+baza_plik = "pracownicy.sqlite3"
+baza = SqliteDatebase(baza-plik)
+
+
 def wyniki(cur):
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
@@ -28,19 +36,19 @@ def kw_2(cur):
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
         print(tuple(row))
-        
+
 def kw_3(cur):
     cur.execute("""
         SELECT   AVG(EgzMat), tbKlasy.Klasa
         FROM tbUczniowie, tbKlasy
         WHERE tbUczniowie.KlasaID = tbKlasy.IDKlasy
         AND Klasa LIKE '1A'
-        
+
     """) #pojedyncze polecenie
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row))       
-        
+        print(tuple(row))
+
 def kw_4(cur):
     cur.execute("""
         SELECT   Imie, Nazwisko, tbOceny.Ocena, tbPrzedmioty.Przedmiot
@@ -51,7 +59,7 @@ def kw_4(cur):
     """) #pojedyncze polecenie
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row))   
+        print(tuple(row))
 
 def kw_5(cur):
     cur.execute("""
@@ -60,30 +68,30 @@ def kw_5(cur):
         WHERE tbOceny.PrzedmiotID = tbPrzedmioty.IDPrzedmiotu
         AND strftime('%m', datad) LIKE 10
         AND Przedmiot LIKE 'fizyka'
-        
-        
+
+
     """) #pojedyncze polecenie
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row))       
-       
-                        
+        print(tuple(row))
+
+
 def kw_c(cur):
     cur.execute("""
-        SELECT siedziba, SUM(placa) AS pensje 
+        SELECT siedziba, SUM(placa) AS pensje
         FROM pracownicy, dzial
         WHERE  pracownicy.id_dzial=dzial.id
-        GROUP BY siedziba 
+        GROUP BY siedziba
         ORDER BY pensje ASC
     """) #pojedyncze polecenie
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
         print(tuple(row))
-        
+
 def kw_e(cur):
     cur.execute("""
         SELECT nazwisko, stanowisko,
-        pracownicy.placa * 
+        pracownicy.placa *
         (SELECT premia.premia
         FROM premia
         WHERE pracownicy.stanowisko = premia.id)
@@ -94,46 +102,46 @@ def kw_e(cur):
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
         print(tuple(row))
-        
+
 def kw_f(cur):
     cur.execute("""
         SELECT AVG(placa)
         FROM pracownicy
         WHERE imie LIKE ('%a')
     """)
-    
+
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row)) 
-        
+        print(tuple(row))
+
     cur.execute("""
         SELECT AVG(placa)
         FROM pracownicy
         WHERE imie NOT LIKE ('%a')
     """)
-    
+
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row)) 
-          
+        print(tuple(row))
+
 def kw_g(cur):
     cur.execute("""
         SELECT imie, nazwisko, stanowisko,
         (JulianDay())
-        FROM 
-        WHERE 
+        FROM
+        WHERE
     """)
-    
+
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row)) 
-              
+        print(tuple(row))
+
 def kw_d(cur):
     nazwa1 = input("Podaj nazwę działu: ")
     nazwa2= input("Podaj nazwę siedziby: ")
     print(nazwa1)
     print(nazwa2)
-    
+
     cur.execute("""
         SELECT nazwisko, imie, dzial.id, dzial.nazwa, dzial.siedziba
         FROM pracownicy, dzial
@@ -144,20 +152,20 @@ def kw_d(cur):
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
         print(tuple(row))
-    
+
 def dodaj():
     cur.execute("""
         INSERT INTO tbKlasy
         VALUES (?,?,?,?)
     """, [None, '3C', 2015, 2017])
-    
+
 def aktualizuj(cur):
     cur.execute("""
         UPDATE tbKlasy
         SET klasa = ?
         WHERE idklasy = ?
     """, [ '3D', 13])
-    
+
 def usun(cur):
     cur.execute('DELETE FROM tbKlasy WHERE klasa = ? AND roknaboru = ?', ['3B', 2015])
 
@@ -167,21 +175,21 @@ def aktualizuj_2(cur):
         UPDATE tbUczniowie
         SET EgzJez = ?
         WHERE IDklasy = ?
-    """, [ 'Paulina Dziedzic', 35]) 
-    
-    
+    """, [ 'Paulina Dziedzic', 35])
+
+
 def main(args):
     con = sqlite3. connect('szkola.db')
     cur = con.cursor() #utworzenie kursora
     con.row_factory = sqlite3.Row
-    
+
     #dodaj(cur)
     #aktualizuj(cur)
     #usun(cur)
     aktualizuj_2(cur)
     con.commit()
     wyniki(cur.execute('SELECT * FROM tbKlasy'))
-    
+
     #kw_d(cur)
    # kw_c(cur)
     #kw_e(cur)
@@ -191,8 +199,8 @@ def main(args):
     #kw_3(cur)
     #kw_4(cur)
     #kw_5(cur)
-    
-    
+
+
     return 0
 
 if __name__ == '__main__':
--- bazy/sqlite3_2/szkola_od_Maria_Niemiec.py
+++ bazy/sqlite3_2/szkola_od_Maria_Niemiec.py
@@ -4,6 +4,59 @@
 
 import sqlite3
 
+import os
+from peewee import *
+from dane import *
+
+baza_plik = "pracownicy.sqlite3"
+baza = SqliteDatebase(baza_plik)
+
+class BazaModel(Model):
+    class Meta:
+        database = baza
+
+class Klasa(BazaModel):
+    id = IntegerField(primary_key=True)
+    klasa = CharField(null=False)
+    rok_naboru = IntegerField(null=False)
+    rok_matury = IntegerField(null=False)
+
+class Przedmiot(BazaModel):
+    id = IntegerField(primary_key=True)
+    przedmiot = CharField(null=False)
+    nazwisko_naucz = CharField(null=False)
+    imie_naucz = CharField(null=False)
+    plec_naucz = IntegerField(null=False)
+
+class Ocena(BazaModel):
+    id = IntegerField(primery_key=True)
+    datad = DateField(null=False)
+    uczen = IntegerField(null=True)
+    przedmiot = IntegerField(null=True)
+    ocena = DecimalField(null=True)
+    UczenID= ForeignKeyField(Uczen, related_name='szkola')
+    PrzedmiotID = ForeignKeyField(Pzredmiot, related_name='szkola')
+
+class Uczen(BazaModel):
+    id = IntegerField(primary_key=True)
+    imie = CharFiel(null=False)
+    nazwisko = CharFiel(null=False)
+    plec = IntegerField(null=False)
+    klasa = IntegerField(null=True)
+    egzhum =
+    egzmat =
+    egzjez =
+    KlasaD = ForeignKeyField(Klasa, related_name='szkola')
+
+
+
+
+
+
+
+
+baza.connect()
+
 def kw_1(cur):
     cur.execute("""
         SELECT nazwisko, imie, tbKlasy.Klasa
@@ -23,19 +76,19 @@ def kw_2(cur):
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
         print(tuple(row))
-        
+
 def kw_3(cur):
     cur.execute("""
         SELECT   AVG(EgzMat), tbKlasy.Klasa
         FROM tbUczniowie, tbKlasy
         WHERE tbUczniowie.KlasaID = tbKlasy.IDKlasy
         AND Klasa LIKE '1A'
-        
+
     """) #pojedyncze polecenie
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row))       
-        
+        print(tuple(row))
+
 def kw_4(cur):
     cur.execute("""
         SELECT   Imie, Nazwisko, tbOceny.Ocena, tbPrzedmioty.Przedmiot
@@ -46,7 +99,7 @@ def kw_4(cur):
     """) #pojedyncze polecenie
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row))   
+        print(tuple(row))
 
 def kw_5(cur):
     cur.execute("""
@@ -55,30 +108,30 @@ def kw_5(cur):
         WHERE tbOceny.PrzedmiotID = tbPrzedmioty.IDPrzedmiotu
         AND strftime('%m', datad) LIKE 10
         AND Przedmiot LIKE 'fizyka'
-        
-        
+
+
     """) #pojedyncze polecenie
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row))       
-       
-                        
+        print(tuple(row))
+
+
 def kw_c(cur):
     cur.execute("""
-        SELECT siedziba, SUM(placa) AS pensje 
+        SELECT siedziba, SUM(placa) AS pensje
         FROM pracownicy, dzial
         WHERE  pracownicy.id_dzial=dzial.id
-        GROUP BY siedziba 
+        GROUP BY siedziba
         ORDER BY pensje ASC
     """) #pojedyncze polecenie
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
         print(tuple(row))
-        
+
 def kw_e(cur):
     cur.execute("""
         SELECT nazwisko, stanowisko,
-        pracownicy.placa * 
+        pracownicy.placa *
         (SELECT premia.premia
         FROM premia
         WHERE pracownicy.stanowisko = premia.id)
@@ -89,46 +142,46 @@ def kw_e(cur):
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
         print(tuple(row))
-        
+
 def kw_f(cur):
     cur.execute("""
         SELECT AVG(placa)
         FROM pracownicy
         WHERE imie LIKE ('%a')
     """)
-    
+
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row)) 
-        
+        print(tuple(row))
+
     cur.execute("""
         SELECT AVG(placa)
         FROM pracownicy
         WHERE imie NOT LIKE ('%a')
     """)
-    
+
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row)) 
-          
+        print(tuple(row))
+
 def kw_g(cur):
     cur.execute("""
         SELECT imie, nazwisko, stanowisko,
         (JulianDay())
-        FROM 
-        WHERE 
+        FROM
+        WHERE
     """)
-    
+
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
-        print(tuple(row)) 
-              
+        print(tuple(row))
+
 def kw_d(cur):
     nazwa1 = input("Podaj nazwę działu: ")
     nazwa2= input("Podaj nazwę siedziby: ")
     print(nazwa1)
     print(nazwa2)
-    
+
     cur.execute("""
         SELECT nazwisko, imie, dzial.id, dzial.nazwa, dzial.siedziba
         FROM pracownicy, dzial
@@ -139,12 +192,12 @@ def kw_d(cur):
     wyniki = cur.fetchall() # pobierz wszystkie wiersze od razu
     for row in wyniki:
         print(tuple(row))
-    
+
 def main(args):
     con = sqlite3. connect('szkola.db')
     cur = con.cursor() #utworzenie kursora
     con.row_factory = sqlite3.Row
-    
+
     #kw_d(cur)
    # kw_c(cur)
     #kw_e(cur)
@@ -154,7 +207,7 @@ def main(args):
     #kw_3(cur)
     #kw_4(cur)
     kw_5(cur)
-    
+
     return 0
 
 if __name__ == '__main__':
