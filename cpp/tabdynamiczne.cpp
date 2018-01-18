3ecf370cde2d368d2f0e599f468d74834e8f4063
Mery18 <marianiemiec@wp.pl>
2018-01-18 11:54:28 +0100

    tablice

--- cpp/tabdynamiczne.cpp
+++ cpp/tabdynamiczne.cpp
@@ -6,6 +6,8 @@
 
 
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
 using namespace std;
 
 void wprowadzDane(int *t, int ile ) {
@@ -36,10 +38,47 @@ int tab1W() {
     }
  return  0;   
 }
+ 
+void wypelnij2W(int **tab, int w, int k){
+        srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
+        for(int i = 0; i < w; i++){
+            for(int j=0; j<k; j++){
+                //cout << i << j <<endl;
+                tab[i][j] = rand() % 101;
+                cout << setw(4) << tab[i][j];
+            }
+        }
+    
+}
+ 
+int tab2W(){
+    int w, k, i;
+    cout << "ile wierszy i kolumn? ";
+    cin >> w >> k;
+    int **tab; // deklaracja wskaźnika do wskaźnika
+    
+     try {
+        tab = new int *[w]; // utworzenie tablicy wskaźnikow
+    } catch(bad_alloc) {
+        cout << "Za malo pamięci!";
+        return 1;
+    }
+    for(i = 0; i < w; i++){
+        try {
+            tab[i] = new int [k];
+        } catch(bad_alloc) {
+            cout << "Za malo pamięci!";
+            return 1;
+        }
+    }
+    wypelnij2W(tab, w, k);
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
-    tab1W();
+    tab2W();
+    //tab1W();
     
 	return 0;
 }
